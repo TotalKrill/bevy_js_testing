9ff1be9fcf04b78f8332d7fe67ce97e6690987ef
Kristoffer Ödmark <kristoffer.odmark90@gmail.com>
2023-03-24 15:34:14 +0100

    libify it

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,111 @@ use once_cell::sync::OnceCell;
 use serde::{Deserialize, Serialize};
 
 use wasm_bindgen::prelude::*;
-/// Wrapper around Receiver, just to derive [`Resource`].
-#[derive(Resource)]
-struct ReceiverResource<T> {
-    rx: Receiver<T>,
+
+mod web_communication {
+    use std::marker::PhantomData;
+
+    use async_std::channel::{unbounded, Receiver, Sender};
+    use bevy::{ecs::event::Event, prelude::*};
+    use once_cell::sync::OnceCell;
+    use serde::{de::DeserializeOwned, Serialize};
+    use wasm_bindgen::prelude::*;
+
+    /// Wrapper around Receiver, just to derive [`Resource`].
+    #[derive(Resource)]
+    struct ReceiverResource<T> {
+        rx: Receiver<T>,
+    }
+
+    /// system that sends postMessage to the parent window
+    pub fn postmessage_to_parent<OUTPUT: Send + Sync + Event + Serialize>(
+        mut postevt: EventReader<OUTPUT>,
+    ) {
+        for evt in postevt.iter() {
+            if let Some(window) = web_sys::window() {
+                if let Ok(Some(parent)) = window.parent() {
+                    parent.post_message(&serde_wasm_bindgen::to_value(evt).unwrap(), "*");
+                } else {
+                    window.post_message(&serde_wasm_bindgen::to_value(evt).unwrap(), "*");
+                }
+            }
+        }
+    }
+    pub fn postmessage_input<INPUT: Send + Sync + Event>(
+        rx: Res<ReceiverResource<INPUT>>,
+        evt: EventWriter<INPUT>,
+    ) {
+        if let Ok(rx) = rx.rx.try_recv() {
+            evt.send(rx);
+        }
+    }
+
+    use crate::SENDER;
+
+    #[wasm_bindgen]
+    extern "C" {
+        pub async fn js_event(val: JsValue) {
+            if let Ok(cmd) = serde_wasm_bindgen::from_value(val) {
+                if let Some(sender) = SENDER.get() {
+                    sender.send(cmd).await;
+                }
+            }
+        }
+    }
+
+    pub struct WebPostMessage<INPUT: Event, OUTPUT: Event> {
+        sender: &'static OnceCell<Sender<INPUT>>,
+        _input: PhantomData<INPUT>,
+        _output: PhantomData<OUTPUT>,
+    }
+
+    impl<INPUT, OUTPUT> WebPostMessage<INPUT, OUTPUT>
+    where
+        INPUT: Send + Sync + Event,
+        OUTPUT: Send + Sync + Event,
+    {
+        pub fn new(sender: &'static OnceCell<Sender<INPUT>>) -> Self {
+            Self {
+                sender,
+                _input: PhantomData::default(),
+                _output: PhantomData::default(),
+            }
+        }
+    }
+
+    impl<INPUT, OUTPUT> Plugin for WebPostMessage<INPUT, OUTPUT>
+    where
+        INPUT: Send + Sync + Event + DeserializeOwned,
+        OUTPUT: Send + Sync + Event + Serialize,
+    {
+        fn build(&self, app: &mut App) {
+            let (tx, rx) = unbounded();
+            self.sender.set(tx);
+
+            let cb = Closure::new(|val| js_event(val));
+            let cb = cb.as_ref().unchecked_ref();
+
+            web_sys::window().unwrap().set_onmessage(Some(cb));
+
+            app.insert_resource(ReceiverResource { rx })
+                .add_event::<INPUT>()
+                .add_event::<OUTPUT>()
+                .add_system(postmessage_to_parent::<OUTPUT>)
+                .add_system(postmessage_input::<INPUT>);
+        }
+    }
+}
+static SENDER: OnceCell<Sender<Command>> = OnceCell::new();
+
+use web_communication::*;
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(WebPostMessage::<Command, PostEvent>::new(&SENDER))
+        .add_startup_system(setup)
+        .add_system(sprite_movement)
+        .add_system(toggle_from_js)
+        .run();
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -25,39 +126,13 @@ pub enum PostEvent {
     AFloat(f32),
 }
 
-/// system that sends postMessage to the parent window
-pub fn postmessage_to_parent(mut postevt: EventReader<PostEvent>) {
-    for evt in postevt.iter() {
-        if let Some(window) = web_sys::window() {
-            if let Ok(Some(parent)) = window.parent() {
-                parent.post_message(&serde_wasm_bindgen::to_value(evt).unwrap(), "*");
-            } else {
-                window.post_message(&serde_wasm_bindgen::to_value(evt).unwrap(), "*");
-            }
-        }
-    }
+#[derive(Component)]
+enum Direction {
+    Up,
+    Down,
 }
-
-#[wasm_bindgen]
-pub async fn js_event(val: JsValue) {
-    let cmd: Command = serde_wasm_bindgen::from_value(val).unwrap();
-
-    match cmd {
-        Command::Toggle => {
-            if let Some(sender) = SENDER.get() {
-                sender.send(cmd).await;
-            }
-        }
-        Command::Print(s) => {
-            web_sys::console::log_1(&s.into());
-        }
-    }
-}
-
-static SENDER: OnceCell<Sender<Command>> = OnceCell::new();
-
-fn toggle_js(rx: Res<ReceiverResource<Command>>, mut sprite_position: Query<&mut Direction>) {
-    if let Ok(Command::Toggle) = rx.rx.try_recv() {
+fn toggle_from_js(rx: EventReader<Command>, mut sprite_position: Query<&mut Direction>) {
+    for rx in rx.iter() {
         for mut logo in &mut sprite_position {
             match *logo {
                 Direction::Up => *logo = Direction::Down,
@@ -65,27 +140,6 @@ fn toggle_js(rx: Res<ReceiverResource<Command>>, mut sprite_position: Query<&mut
             }
         }
     }
-}
-
-fn main() {
-    let (tx, rx) = unbounded();
-    SENDER.set(tx);
-
-    App::new()
-        .insert_resource(ReceiverResource { rx })
-        .add_plugins(DefaultPlugins)
-        .add_event::<PostEvent>()
-        .add_startup_system(setup)
-        .add_system(sprite_movement)
-        .add_system(toggle_js)
-        .add_system(postmessage_to_parent)
-        .run();
-}
-
-#[derive(Component)]
-enum Direction {
-    Up,
-    Down,
 }
 
 fn setup(
