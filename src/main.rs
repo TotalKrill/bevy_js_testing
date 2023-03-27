2a808e3498949f63513b7ee04e3d130c7e1dbae6
Kristoffer Ödmark <kristoffer.odmark90@gmail.com>
2023-03-27 16:58:30 +0200

    try stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 //! Renders a 2D scene containing a single, moving sprite.
 use std::collections::HashMap;
 
-use async_std::channel::{unbounded, Receiver, Sender};
+// use std::sync::mpsc::{channel, Receiver, Sender};
+// use async_std::channel::{unbounded, Receiver, Sender};
+use crossbeam::channel::{unbounded, Receiver, Sender};
+
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 use once_cell::sync::OnceCell;
 use serde::{Deserialize, Serialize};
@@ -11,15 +14,19 @@ use wasm_bindgen::prelude::*;
 mod web_communication {
     use std::marker::PhantomData;
 
-    use async_std::channel::{unbounded, Receiver, Sender};
+    // use std::sync::mpsc::{channel as unbounded, Receiver, Sender};
+    // use async_std::channel::{unbounded, Receiver, Sender};
+    use crossbeam::channel::{unbounded, Receiver, Sender};
+
     use bevy::{ecs::event::Event, prelude::*};
     use once_cell::sync::OnceCell;
     use serde::{de::DeserializeOwned, Serialize};
     use wasm_bindgen::prelude::*;
+    use web_sys::console;
 
     /// Wrapper around Receiver, just to derive [`Resource`].
     #[derive(Resource)]
-    struct ReceiverResource<T> {
+    pub struct ReceiverResource<T> {
         rx: Receiver<T>,
     }
 
@@ -29,38 +36,42 @@ mod web_communication {
     ) {
         for evt in postevt.iter() {
             if let Some(window) = web_sys::window() {
-                if let Ok(Some(parent)) = window.parent() {
-                    parent.post_message(&serde_wasm_bindgen::to_value(evt).unwrap(), "*");
+                if let Ok(Some(parent)) = window.top() {
+                    // gloo::console::log!(format!("{name:?}"));
+                    if parent != window {
+                        parent
+                            .post_message(&serde_wasm_bindgen::to_value(evt).unwrap(), "*")
+                            .ok();
+                    }
                 } else {
-                    window.post_message(&serde_wasm_bindgen::to_value(evt).unwrap(), "*");
+                    // window.post_message(&serde_wasm_bindgen::to_value(evt).unwrap(), "*");
                 }
             }
         }
     }
     pub fn postmessage_input<INPUT: Send + Sync + Event>(
         rx: Res<ReceiverResource<INPUT>>,
-        evt: EventWriter<INPUT>,
+        mut evt: EventWriter<INPUT>,
     ) {
         if let Ok(rx) = rx.rx.try_recv() {
             evt.send(rx);
         }
     }
 
-    use crate::SENDER;
-
-    #[wasm_bindgen]
-    extern "C" {
-        pub async fn js_event(val: JsValue) {
-            if let Ok(cmd) = serde_wasm_bindgen::from_value(val) {
-                if let Some(sender) = SENDER.get() {
-                    sender.send(cmd).await;
-                }
-            }
-        }
-    }
+    // #[wasm_bindgen]
+    // pub fn js_event(val: JsValue) {
+    //     match serde_wasm_bindgen::from_value(val) {
+    //         Ok(cmd) => {
+    //             if let Some(sender) = SENDER.get() {
+    //                 sender.send(cmd);
+    //                 // sender.send(cmd).await;
+    //             }
+    //         }
+    //         Err(e) => (console::log_1(&"fail to deserialize".into())),
+    //     }
+    // }
 
     pub struct WebPostMessage<INPUT: Event, OUTPUT: Event> {
-        sender: &'static OnceCell<Sender<INPUT>>,
         _input: PhantomData<INPUT>,
         _output: PhantomData<OUTPUT>,
     }
@@ -70,9 +81,8 @@ mod web_communication {
         INPUT: Send + Sync + Event,
         OUTPUT: Send + Sync + Event,
     {
-        pub fn new(sender: &'static OnceCell<Sender<INPUT>>) -> Self {
+        pub fn new() -> Self {
             Self {
-                sender,
                 _input: PhantomData::default(),
                 _output: PhantomData::default(),
             }
@@ -85,13 +95,29 @@ mod web_communication {
         OUTPUT: Send + Sync + Event + Serialize,
     {
         fn build(&self, app: &mut App) {
-            let (tx, rx) = unbounded();
-            self.sender.set(tx);
+            let (tx, rx) = unbounded::<INPUT>();
+            // self.sender.set(tx);
 
-            let cb = Closure::new(|val| js_event(val));
-            let cb = cb.as_ref().unchecked_ref();
+            // let handler = Box::new(js_event) as Box<dyn Fn(_)>;
+            // let closure = Closure::wrap(handler);
+            // let js_func = closure.as_ref().unchecked_ref();
+            // web_sys::window().unwrap().set_onmessage(Some(js_func));
+            // closure.forget();
 
-            web_sys::window().unwrap().set_onmessage(Some(cb));
+            gloo::events::EventListener::new(
+                &web_sys::window().unwrap().into(),
+                "message",
+                move |event| {
+                    let event = event.dyn_ref::<web_sys::MessageEvent>().unwrap_throw();
+                    match serde_wasm_bindgen::from_value::<INPUT>(event.data()) {
+                        Ok(cmd) => {
+                            tx.send(cmd);
+                        }
+                        Err(e) => gloo::console::log!(format!("{e:?}")),
+                    }
+                },
+            )
+            .forget();
 
             app.insert_resource(ReceiverResource { rx })
                 .add_event::<INPUT>()
@@ -101,13 +127,12 @@ mod web_communication {
         }
     }
 }
-static SENDER: OnceCell<Sender<Command>> = OnceCell::new();
 
 use web_communication::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(WebPostMessage::<Command, PostEvent>::new(&SENDER))
+        .add_plugin(WebPostMessage::<Command, PostEvent>::new())
         .add_startup_system(setup)
         .add_system(sprite_movement)
         .add_system(toggle_from_js)
@@ -131,7 +156,7 @@ enum Direction {
     Up,
     Down,
 }
-fn toggle_from_js(rx: EventReader<Command>, mut sprite_position: Query<&mut Direction>) {
+fn toggle_from_js(mut rx: EventReader<Command>, mut sprite_position: Query<&mut Direction>) {
     for rx in rx.iter() {
         for mut logo in &mut sprite_position {
             match *logo {
